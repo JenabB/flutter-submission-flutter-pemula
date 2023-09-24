54ddcdf9e32490e1d1a7448ccb66c7f159f80e18
JenabB <birulangit199^@gmail.com>
2023-09-24 19:21:56 +0700

    add animation and remove hero

--- lib/page/home/home_info.dart
+++ lib/page/home/home_info.dart
@@ -1,59 +1,91 @@
 import 'package:flutter/material.dart';
 
-class HomeInfo extends StatelessWidget {
+class HomeInfo extends StatefulWidget {
   const HomeInfo({Key? key}) : super(key: key);
+
+  @override
+  State<HomeInfo> createState() => _HomeInfoState();
+}
+
+class _HomeInfoState extends State<HomeInfo> {
+  double opacityLevel = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    animateOpacity();
+  }
+
+  void animateOpacity() {
+    Future.delayed(Duration(seconds: 1), () {
+      setState(() {
+        opacityLevel = 1.0;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: 120.0,
+      height: 140.0,
       decoration: BoxDecoration(
         color: Colors.blue,
         borderRadius: BorderRadius.circular(10.0),
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Flexible(
-            child: Container(
-              padding: EdgeInsets.fromLTRB(18, 14, 10, 10),
-              child: Column(
-                children: [
-                  Text(
-                    "Find Your Perfect Home",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const Text(
-                    "Enjoy Exclusive Deals",
-                    style: TextStyle(color: Colors.white, fontSize: 12.0),
-                  ),
-                  const Text(
-                    "At Local Hotspots",
-                    style: TextStyle(color: Colors.white, fontSize: 12.0),
-                  ),
-                  ElevatedButton(
-                    onPressed: null,
-                    style: ElevatedButton.styleFrom(
-                        minimumSize: Size(10, 20), backgroundColor: Colors.red),
-                    child: Text(
-                      "Check Now",
-                      style: TextStyle(color: Colors.white, fontSize: 10.0),
-                    ),
-                  ),
-                ],
-              ),
+          AnimatedOpacity(
+            duration: Duration(seconds: 1),
+            opacity: opacityLevel,
+            child: Text(
+              "Find Your Perfect Home",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0),
             ),
           ),
-          Expanded(
+          SizedBox(
+            height: 3,
+          ),
+          AnimatedOpacity(
+            duration: Duration(seconds: 2),
+            opacity: opacityLevel,
+            child: const Text(
+              "Enjoy Exclusive Deals",
+              style: TextStyle(color: Colors.white, fontSize: 14.0),
+            ),
+          ),
+          SizedBox(height: 3),
+          AnimatedOpacity(
+            duration: Duration(seconds: 3),
+            opacity: opacityLevel,
+            child: const Text(
+              "At Local Hotspots",
+              style: TextStyle(color: Colors.white, fontSize: 11.0),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          AnimatedOpacity(
+            duration: Duration(seconds: 3),
+            opacity: opacityLevel,
             child: SizedBox(
-              width: 200,
-              child: Image.asset(
-                "images/hero.png",
-                fit: BoxFit.contain,
+              width: 80,
+              height: 30,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.all(2.0),
+                  backgroundColor: Colors.black,
+                ),
+                child: Text(
+                  "Check Now",
+                  style: TextStyle(color: Colors.white, fontSize: 10.0),
+                ),
               ),
             ),
           ),
