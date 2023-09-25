30c722db48b1fb932e4b17905ec82cba40820cd3
JenabB <birulangit199^@gmail.com>
2023-09-25 09:35:06 +0700

    fix overflow

--- lib/main_profile.dart
+++ lib/main_profile.dart
@@ -14,27 +14,29 @@ class ProfileScreen extends StatelessWidget {
       child: Center(
         child: SizedBox(
           width: containerWidth,
-          child: Container(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              children: [
-                Align(
-                  alignment: Alignment.center,
-                  child: Container(
-                    margin: const EdgeInsets.all(12),
-                    child: const Text(
-                      "Profile",
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          child: SingleChildScrollView(
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.center,
+                    child: Container(
+                      margin: const EdgeInsets.all(12),
+                      child: const Text(
+                        "Profile",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
                     ),
                   ),
-                ),
-                ProfileHeader(),
-                SizedBox(height: 10),
-                ProfileActivity(),
-                SizedBox(height: 10),
-                ProfileOptions()
-              ],
+                  ProfileHeader(),
+                  SizedBox(height: 10),
+                  ProfileActivity(),
+                  SizedBox(height: 10),
+                  ProfileOptions(),
+                ],
+              ),
             ),
           ),
         ),
